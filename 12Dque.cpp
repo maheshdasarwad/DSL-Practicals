90d7985fa2e3459e41dfc4a06e3e31ac9a1aa644
maheshdasarwad <maheshdasarwad855@gmail.com>
2024-12-11 07:45:40 +0530

    Update 12Dque.cpp

--- 12Dque.cpp
+++ 12Dque.cpp
@@ -60,7 +60,6 @@ class Deque
                 cout<<"\nDequeue Underflow..!"<<endl;
                 return;
             }
-            int i = 0;
             for (int i = 0; i < size - 1; i++)
             {
                 arr[i] = arr[i + 1];
